76fbace3c3a5cc0fde77d43ff2d7b6632b6fb891
saadrasheeddev <saadrasheeddev@gmail.com>
2021-06-09 21:03:02 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Friend from './friend.js'
+
+function App() {
+  return (
+    <div class="text">
+      <Friend tripTo = "Thatta" collegeFriend = "Ammar" schoolFriend = "Owais"></Friend>
+      <hr></hr>
+      <Friend tripTo = "Iftikhar Olympian Ground" collegeFriend = "Haris" schoolFriend = "Burhan"></Friend>
+      <hr></hr>
+      <Friend tripTo = "Behria Theme Park" collegeFriend = "Hassan" schoolFriend = "Anas"></Friend>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/friend.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Friend(props){
+    return(
+        <h1>I went to {props.tripTo} with {props.collegeFriend} and {props.schoolFriend}</h1>
+    );
+}
+
+export default Friend;
